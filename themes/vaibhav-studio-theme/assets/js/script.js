5bb4c9fa4783dc824745efe40f81d3b35fc093d7
Vaibhav Dhiman <vaibhavsys@protonmail.com>
2023-02-24 16:04:18 +0530

    Revert "test: remark42 comments"
    This reverts commit 07e609752051fa1736b57edbe5f94b4a68b4f3b1.

--- themes/vaibhav-studio-theme/assets/js/script.js
+++ themes/vaibhav-studio-theme/assets/js/script.js
@@ -4,17 +4,3 @@ document.querySelectorAll(".nav-link").forEach(link => {
         link.classList.add("active");
     }
 });
-
-var remark_config = {
-    host: 'https://remark42.vaibhav.studio',
-    site_id: 'remark',
-    components: ['embeded', 'last-comments'],
-    max_shown_comments: 100,
-    theme: 'dark',
-    locale: 'es',
-    show_email_subscription: true,
-    simple_view: false,
-    no_footer: false
-  }
-
-!function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
